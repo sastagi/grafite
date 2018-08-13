24773f648eed3baf15e227e85ec8e5b528ea12c9
Santosh <sastagi@gmail.com>
2018-08-12 20:59:31 -0700

    Using configurable gcloud values

--- README.md
+++ README.md
@@ -6,12 +6,13 @@
 2. Add the google-services.json to the root of your project.
 3. Install gcloud - https://cloud.google.com/sdk/docs/downloads-interactive
 4. Set the gcloud project to the Firebase project created in step 1. (View all projects using `gcloud projects list` and set the gcloud project using `gcloud config set project `project_id_for_firebase_project
-5. Go to gcloud console on web to create a new bucket called.
-6. Specify this bucket name in the config.properties file.
-7. Specify this bucket name for BUCKET_NAME in flank.sh
+5. Go to gcloud console on web to create a new bucket. (more information about buckets here - https://cloud.google.com/storage/docs/creating-buckets)
+6. Create a directory
+6. Specify this bucket and the directory in the config.properties file.
+7. Specify this bucket and directory for BUCKET_NAME in flank.sh.
 
 ## Creating coverage reports
 
-Run the following scripts:
+Run the following from the root of the project:
 1. `./scripts/apks_for_testing.sh`
 2. `./scripts/flank.sh`
--- scripts/flank.sh
+++ scripts/flank.sh
@@ -1,21 +1,12 @@
 #!/usr/bin/env bash
 
-BUCKET_NAME=coverage_stuff #Put your bucket name here
-DIR_NAME=coverage
+BUCKET_NAME=coverage_stuff #Put bucket name here
+DIR_NAME=$(($(date +%s)))
 
-## Fetch flank.test
-gsutil -q stat gs://${BUCKET_NAME}/flank.tests
+sed -i.bak '/gcloud-bucket/d' config.properties
+echo "gcloud-bucket: ${BUCKET_NAME}/${DIR_NAME}" >> config.properties
+rm config.properties.bak
 
-return_value=$?
-
-if [ ${return_value} == 0 ]; then
-    echo "flank.tests found"
-    gsutil cp gs://${BUCKET_NAME}/flank.tests .
-else
-    echo "flank.tests not found"
-fi
-
-## Remove old coverage reports
 rm -rf misc/coverage_reports/*.exec
 
 gsutil cp config.properties  gs://${BUCKET_NAME}/${DIR_NAME}/
@@ -25,12 +16,12 @@ java -jar misc/Flank-2.0.1.jar -a app/build/outputs/apk/debug/app-debug.apk -t a
 REPORT_COUNT=$(gsutil ls gs://${BUCKET_NAME}/${DIR_NAME}/ | wc -l)
 for number in `seq 0 ${REPORT_COUNT}`
 do
-    gsutil -q stat gs://${BUCKET_NAME}/coverage/${number}/*
+    gsutil -q stat gs://${BUCKET_NAME}/${DIR_NAME}/${number}/*
 
     return_value=$?
 
     if [ ${return_value} == 0 ]; then
-        test_report_directory_path_all_devices=$(gsutil ls gs://${BUCKET_NAME}/coverage/${number}/* | grep -e "/$")
+        test_report_directory_path_all_devices=$(gsutil ls gs://${BUCKET_NAME}/${DIR_NAME}/${number}/* | grep -e "/$")
         test_report_directory_path_single_device=(${test_report_directory_path_all_devices})
         download_path_for_coverage_report=${test_report_directory_path_single_device[0]}coverage.ec
         echo "Downloading coverage reports from: ${download_path_for_coverage_report}"
@@ -38,9 +29,3 @@ do
     fi
 
 done
-
-gsutil mv gs://${BUCKET_NAME}/${DIR_NAME}/flank.tests gs://${BUCKET_NAME}/
-
-gsutil -m rm -r gs://${BUCKET_NAME}/${DIR_NAME}/
-
-rm flank.tests
